d0ed0ae302a3fc2747e870e9dc7ba44d85e69f9a
MAKS-1211 <mihir.kumar@mail.mcgill.ca>
2023-11-11 19:29:53 -0500

    Changed structure for running tests in a random order

--- Part_2/steps/post_new_project.py
+++ Part_2/steps/post_new_project.py
@@ -13,6 +13,7 @@ def step_impl(context):
     context.jar_process = subprocess.Popen(['java', '-jar', context.api_path])
     time.sleep(5)
 
+
 @step("the API has no project entries")
 def step_impl(context):
     response = requests.get("http://localhost:4567/projects")
@@ -20,21 +21,24 @@ def step_impl(context):
         deleted_id = str(data_points['id'])
         requests.delete("http://localhost:4567/projects/" + deleted_id)
 
+
 @given('A project with a title {tit}, a description {desc}, an active status {act} and a completed status {comp}')
 def step_impl(context, tit, desc, act, comp):
     context.project = {
-            "completed": bool(comp),
-            "active": bool(act),
-            "description": desc,
-            "title": tit
-        }
+        "completed": bool(comp),
+        "active": bool(act),
+        "description": desc,
+        "title": tit
+    }
+
 
 @given("A project with no title, no description, no active status and no completed status")
 def step_impl(context):
     context.project = {}
 
 
-@given("A project with a title {tit}, a description {desc}, an active status {act}, a completed status {comp} and an id {id}")
+@given(
+    "A project with a title {tit}, a description {desc}, an active status {act}, a completed status {comp} and an id {id}")
 def step_impl(context, tit, desc, act, comp, id):
     context.project = {
         "completed": bool(comp),
@@ -44,13 +48,16 @@ def step_impl(context, tit, desc, act, comp, id):
         "id": id
     }
 
+
 @when('The user posts the project')
 def step_impl(context):
     context.response = requests.post("http://localhost:4567/projects", data=json.dumps(context.project))
 
+
 @then('The user receives a CREATED HTTP status code (201)')
 def step_impl(context):
     assert context.response.status_code == 201
+
 
 @then('The user receives the project back in the response')
 def step_impl(context):
@@ -60,7 +67,8 @@ def step_impl(context):
     assert str(context.response.json()['description']).upper() == str(context.project['description']).upper()
 
 
-@step('The user receives a default project back in the response with title = "", description = "", completed = False, active = False')
+@step(
+    'The user receives a default project back in the response with title = "", description = "", completed = False, active = False')
 def step_impl(context):
     assert str(context.response.json()['completed']).upper() == "FALSE"
     assert str(context.response.json()['active']).upper() == "FALSE"
